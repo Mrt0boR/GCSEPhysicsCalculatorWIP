d9d1259b45b39ef59e31a31b88cc9ac5e63a6a09
James Bailey <james.a.bailey16@gmail.com>
2023-06-27 00:06:43 +0100

    start button; orientation + functionality issues

--- mainmenugui.java
+++ mainmenugui.java
@@ -1,11 +1,10 @@
 package Mainmenu;
+
 import java.awt.*;
-import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -39,7 +38,7 @@ class rungui extends JFrame {
 
             //layout setup -- constraints allows the gridx and y refs
             GridBagLayout mm = new GridBagLayout();
-            GridBagConstraints gbcmain = new GridBagConstraints();
+            GridBagConstraints welcomepanelgbc = new GridBagConstraints();
             
             //Menu Label
             JLabel welcomemess = new JLabel("Welcome to Velocity");
@@ -49,9 +48,28 @@ class rungui extends JFrame {
             welcomemess.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             
             //label placement
-            gbcmain.gridx = 21;
-            gbcmain.gridy = 37;
-            panel.add(welcomemess,gbcmain);
+            welcomepanelgbc.gridx = 0;
+            welcomepanelgbc.gridy = 0;
+            panel.add(welcomemess,welcomepanelgbc);
+
+            //Enter button to take you to the Velocity app
+            JButton enterv = new JButton("Enter Velocity", null);
+            welcomepanelgbc.gridx = 0;
+            welcomepanelgbc.gridy = 0;
+            // need to find a way to make the button be horizontal to the panel, maybe im not using GBC properly?
+            
+            
+            ActionListener appinit = new ActionListener() {
+                
+                public void actionPerformed(ActionEvent appinit) {
+                    Mainmenu.Velocity.Velocity();
+                
+                }
+            };
+            
+            enterv.addActionListener(appinit);
+            panel.add(enterv);
+
 
             //Taskbar
 
@@ -60,8 +78,8 @@ class rungui extends JFrame {
             //New JMenu
             JMenu fileMenu = new JMenu("File");
             
-            //new MenuItems
-            JMenuItem exititem = new JMenuItem("Exit");
+            //new MenuItems and action listener for exit functionalitu from File/Exit
+            JMenuItem exititem = new JMenuItem("Exit Velocity");
             exititem.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent closegui) {
                     //Close the JFrame when the exit on JMenu item is clicked

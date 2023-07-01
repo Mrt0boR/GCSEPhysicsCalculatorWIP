080716b31c4a018d96702963f616dda67706914f
faris-hamdy <faris.hamdy@virignmedia.com>
2023-07-01 20:03:23 +0100

    added seamless page switch functionality

--- Calculator/MainMenu.java
+++ Calculator/MainMenu.java
@@ -5,13 +5,13 @@ import java.awt.event.*;
 
 public class MainMenu implements ActionListener {
 
+    public static JFrame frame = new JFrame("Calculator");
     // constructor - good practice to include rather than loading up the main method
     public MainMenu() {
         GUISetup();
     }
 
     public void GUISetup() {
-        JFrame frame = new JFrame("Calculator");
         JPanel panel = new JPanel(new GridBagLayout());
         Color backgroundcustomcolour = new Color(198,198,198);
         panel.setBackground(backgroundcustomcolour);
@@ -57,19 +57,80 @@ public class MainMenu implements ActionListener {
         gbc.gridx = 1;
         gbc.gridy = 1;
         panel.add(speedModuleButton, gbc);
+        
         speedModuleButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent speedModulePressed) {
                 //Close the JFrame when the exit on JMenu item is clicked
-                new SpeedModule();
+                frame.remove(panel);
+                SpeedModule.pageSwitch();
             }
         });
 
         //frame and panel combination and JFrame parameters
+        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(panel);
         frame.setJMenuBar(menubar);
-        frame.setSize(500, 500);   
+        frame.setSize(500, 500);
         frame.setVisible(true);
+    }
+
+    public static void pageSwitch() {
+        JPanel panel = new JPanel(new GridBagLayout());
+        Color backgroundcustomcolour = new Color(198,198,198);
+        panel.setBackground(backgroundcustomcolour);
+        //panel styling
+
+        //Jcomponents (incl gridbagcontraints)
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        //Menu Label
+        JLabel welcomemess = new JLabel("Welcome to Calculator");
+
+        //Label styling
+        welcomemess.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        welcomemess.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        //label placement
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.weighty = 1;
+        panel.add(welcomemess,gbc);
+
+        //Taskbar
+        JMenuBar menubar = new JMenuBar();
+
+        //New JMenu
+        JMenu fileMenu = new JMenu("File");
+
+        //new MenuItems
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent exitPressed) {
+                //Close the JFrame when the exit on JMenu item is clicked
+                System.exit(0);
+            }
+        });
+
+        menubar.add(fileMenu);
+        fileMenu.add(exitItem);
+
+        JButton speedModuleButton = new JButton("Speed");
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        panel.add(speedModuleButton, gbc);
+        speedModuleButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent speedModulePressed) {
+                //Close the JFrame when the exit on JMenu item is clicked
+                frame.remove(panel);
+                SpeedModule.pageSwitch();
+            }
+        });
+
+        //frame and panel combination and JFrame parameters
+        frame.add(panel);
+        frame.setSize(500, 500);
     }
 
     public static void main(String[] args){

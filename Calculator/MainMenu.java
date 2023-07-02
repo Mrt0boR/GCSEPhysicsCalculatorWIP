405463afe7e48f3a7d818d3f787df78cc875042b
faris-hamdy <faris.hamdy@virignmedia.com>
2023-07-02 11:54:22 +0100

    removed GUISetup method, integrated into pageSwitch

--- Calculator/MainMenu.java
+++ Calculator/MainMenu.java
@@ -8,10 +8,10 @@ public class MainMenu implements ActionListener {
     public static JFrame frame = new JFrame("Calculator");
     // constructor - good practice to include rather than loading up the main method
     public MainMenu() {
-        GUISetup();
+        pageSwitch();
     }
 
-    public void GUISetup() {
+    public static void pageSwitch() {
         JPanel panel = new JPanel(new GridBagLayout());
         Color backgroundcustomcolour = new Color(198,198,198);
         panel.setBackground(backgroundcustomcolour);
@@ -72,65 +72,6 @@ public class MainMenu implements ActionListener {
         frame.setJMenuBar(menubar);
         frame.setSize(500, 500);
         frame.setVisible(true);
-    }
-
-    public static void pageSwitch() {
-        JPanel panel = new JPanel(new GridBagLayout());
-        Color backgroundcustomcolour = new Color(198,198,198);
-        panel.setBackground(backgroundcustomcolour);
-        //panel styling
-
-        //Jcomponents (incl gridbagcontraints)
-        GridBagConstraints gbc = new GridBagConstraints();
-
-        //Menu Label
-        JLabel welcomemess = new JLabel("Welcome to Calculator");
-
-        //Label styling
-        welcomemess.setFont(new Font("Times New Roman", Font.BOLD, 16));
-        welcomemess.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-        //label placement
-        gbc.anchor = GridBagConstraints.PAGE_START;
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.weighty = 1;
-        panel.add(welcomemess,gbc);
-
-        //Taskbar
-        JMenuBar menubar = new JMenuBar();
-
-        //New JMenu
-        JMenu fileMenu = new JMenu("File");
-
-        //new MenuItems
-        JMenuItem exitItem = new JMenuItem("Exit");
-        exitItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent exitPressed) {
-                //Close the JFrame when the exit on JMenu item is clicked
-                System.exit(0);
-            }
-        });
-
-        menubar.add(fileMenu);
-        fileMenu.add(exitItem);
-
-        JButton speedModuleButton = new JButton("Speed");
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        panel.add(speedModuleButton, gbc);
-        speedModuleButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent speedModulePressed) {
-                //Close the JFrame when the exit on JMenu item is clicked
-                frame.remove(panel);
-                SpeedModule.pageSwitch();
-            }
-        });
-
-        //frame and panel combination and JFrame parameters
-        frame.add(panel);
-        frame.setSize(500, 500);
     }
 
     public static void main(String[] args){
